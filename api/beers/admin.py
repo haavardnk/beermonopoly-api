8aba3c8c292ee39eb929d13ea6828378db6bd36d
Håvard Kråkenes <haavardnk@outlook.com>
2020-12-09 10:39:30 +0100

    Added more models and scripts
    - Added models for stores and stock
    - Added script for getting beers from API
    - Added script for getting stores from CSV
    - Added API search

--- api/beers/admin.py
+++ api/beers/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from beers.models import Beer, SiteSetting
+from beers.models import Beer, ExternalAPI, Store, Stock
 
 @admin.register(Beer)
 class BeerAdmin(admin.ModelAdmin):
-    list_display = ("name", "beerid", "untappd_id", "rating", "vinmonopolet_updated", "untappd_updated")
+    list_display = ("name", "brewery", "beerid", "untappd_id", "rating", "vinmonopolet_updated", "untappd_updated")
 
-admin.site.register(SiteSetting)
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ("name", "storeid", "address")
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("store", "beer", "quantity")
+
+admin.site.register(ExternalAPI)
