3cb907337a9f76c2f204e702ff33584962a3fec7
Håvard Kråkenes <haavardnk@outlook.com>
2021-01-09 20:00:31 +0100

    More tests for full coverage of matching algorithm

--- api/beers/tests/test_untappd_matching.py
+++ api/beers/tests/test_untappd_matching.py
@@ -56,6 +56,66 @@ def test_match_correct_beer(mocked_responses):
     assert beer.untpd_id == 100415
     assert beer.untpd_url == "https://untappd.com/b/ayinger-privatbrauerei-ayinger-winter-bock/100415"
 
+@pytest.mark.django_db
+@pytest.mark.filterwarnings('ignore::UserWarning')
+def test_no_match(mocked_responses):
+    beer = Beer.objects.create(vmp_id=12630102, vmp_name="Poppels x Sudden Death Behind The Mask NEIPA", active=True)
+    query = "Poppels Death Behind The Mask NEIPA"
+    url = "https://api.test.com/v4/search/beer?client_id=123&client_secret=321&q="+quote(query)+"&limit=5"
+    mocked_responses.add(responses.GET, url,
+                json={"response":{
+                    "beers":{
+                        "items":[]
+                        }
+                    }
+                },
+                status=200,
+                headers={'X-Ratelimit-Remaining':'10'},
+                content_type='application/json'
+    )
+    
+    out = management.call_command('match_untpd')
+
+    beer = Beer.objects.get(vmp_id=12630102)
+    assert beer.untpd_id == None
+    assert beer.match_manually == True
+
+@pytest.mark.django_db
+@pytest.mark.filterwarnings('ignore::UserWarning')
+def test_bad_match(mocked_responses):
+    beer = Beer.objects.create(vmp_id=12512002, vmp_name="Buxton Single", active=True)
+    query = "Poppels Death Behind The Mask NEIPA"
+    url = "https://api.test.com/v4/search/beer?client_id=123&client_secret=321&q="+quote(beer.vmp_name)+"&limit=5"
+    mocked_responses.add(responses.GET, url,
+                json={"response":{
+                    "beers":{
+                        "items":[{
+                            "beer":{
+                                "bid":1537674,
+                                "beer_name":"Single Barrel Rain Shadow (Bourbon)",
+                                "beer_slug":"buxton-brewery-single-barrel-rain-shadow-bourbon"},
+                                "brewery":{
+                                    "brewery_name":"Buxton Brewery"
+                                }
+                            },
+                            {"beer":{
+                                "bid":3305869,
+                                "beer_name":"Single Barrel Rain Shadow 2019",
+                                "beer_slug":"buxton-brewery-single-barrel-rain-shadow-2019"},
+                                "brewery":{
+                                    "brewery_name":"Buxton Brewery"}
+                                }
+                        ]}}},
+                status=200,
+                headers={'X-Ratelimit-Remaining':'10'},
+                content_type='application/json'
+    )
+    
+    out = management.call_command('match_untpd')
+
+    beer = Beer.objects.get(vmp_id=12512002)
+    assert beer.untpd_id == None
+    assert beer.match_manually == True
 
 @pytest.mark.django_db
 @pytest.mark.filterwarnings('ignore::UserWarning')
@@ -71,6 +131,7 @@ def test_remove_collab_brewery():
 @responses.activate
 def test_remove_beer_type():
     Beer.objects.create(vmp_id=10346602, vmp_name="De Tvende No excuses, all apologies New England IPA", active=True)
+    MatchFilter.objects.create(name="ipa")
     MatchFilter.objects.create(name="new england ipa")
     management.call_command('match_untpd')
 
