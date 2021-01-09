ec8e65f038a69fe1aa047ea304c0fcbcd08b41ad
Håvard Kråkenes <haavardnk@outlook.com>
2021-01-09 19:22:31 +0100

    Added tests for matching algorithm and removed some bugs
    - Removed bug where having a x in the name were counted as collab no matter where, now it has to be a space before and after the x.

--- api/beers/models.py
+++ api/beers/models.py
@@ -82,9 +82,3 @@ class MatchFilter(models.Model):
     def __str__(self):
         return self.name
 
-class MatchFilterCollab(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
