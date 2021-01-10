5c3dd762f152497b9143ab7f66ec1a5476c3a9cb
Håvard Kråkenes <haavardnk@outlook.com>
2021-01-10 15:16:28 +0100

    Remove further bugs and added tests to update untpd and remove inactive

--- api/beers/tests/test_untappd_matching.py
+++ api/beers/tests/test_untappd_matching.py
@@ -6,10 +6,6 @@ from beers.tasks import smart_update_untappd
 @pytest.fixture(autouse=True)
 def setup(db):
     ExternalAPI.objects.create(name="untappd", baseurl="https://api.test.com/v4/", api_client_id="123", api_client_secret="321")
-    untappd = ExternalAPI.objects.get(name='untappd')
-    api_client_id = untappd.api_client_id
-    api_client_secret = untappd.api_client_secret
-    baseurl = untappd.baseurl
 
 @pytest.fixture
 def mocked_responses():
@@ -46,8 +42,7 @@ def test_match_correct_beer(mocked_responses):
                         ]}}},
                 status=200,
                 headers={'X-Ratelimit-Remaining':'10'},
-                content_type='application/json'
-    )
+                content_type='application/json')
     
     smart_update_untappd()
 
@@ -73,8 +68,7 @@ def test_no_match(mocked_responses):
                 },
                 status=200,
                 headers={'X-Ratelimit-Remaining':'10'},
-                content_type='application/json'
-    )
+                content_type='application/json')
     
     smart_update_untappd()
 
@@ -89,7 +83,6 @@ def test_bad_match(mocked_responses):
     Test that a low Levenshtein distance will not match
     """
     beer = Beer.objects.create(vmp_id=12512002, vmp_name="Buxton Single", active=True)
-    query = "Poppels Death Behind The Mask NEIPA"
     url = "https://api.test.com/v4/search/beer?client_id=123&client_secret=321&q="+quote(beer.vmp_name)+"&limit=5"
     mocked_responses.add(responses.GET, url,
                 json={"response":{
@@ -113,8 +106,7 @@ def test_bad_match(mocked_responses):
                         ]}}},
                 status=200,
                 headers={'X-Ratelimit-Remaining':'10'},
-                content_type='application/json'
-    )
+                content_type='application/json')
     
     smart_update_untappd()
 
@@ -149,3 +141,40 @@ def test_remove_beer_type():
     smart_update_untappd()
 
     assert responses.calls[0].request.url == 'https://api.test.com/v4/search/beer?client_id=123&client_secret=321&q=De%20Tvende%20No%20excuses%2C%20all%20apologies&limit=5'
+
+@pytest.mark.django_db
+@pytest.mark.filterwarnings('ignore::UserWarning')
+def test_stop_when_no_api_calls_remaining(mocked_responses):
+    """
+    Tests that the algorithm stops when only 5 api calls left
+    """
+    Beer.objects.create(vmp_id=12611502, vmp_name="Ayinger Winterbock", active=True)
+    Beer.objects.create(vmp_id=12512002, vmp_name="Ayinger Winterbock", active=True)
+    query = "Ayinger Winterbock"
+    url = "https://api.test.com/v4/search/beer?client_id=123&client_secret=321&q="+quote(query)+"&limit=5"
+    mocked_responses.add(responses.GET, url,
+                json={"response":{
+                    "beers":{
+                        "items":[{
+                            "beer":{
+                                "bid":1320463,
+                                "beer_name":"Ayinger Winter Bock (2015)",
+                                "beer_slug":"ayinger-privatbrauerei-ayinger-winter-bock-2015"
+                                },
+                            "brewery":{
+                                "brewery_name":"Ayinger Privatbrauerei"
+                            }
+                        }]
+                    }
+                }},
+                status=200,
+                headers={'X-Ratelimit-Remaining':'5'},
+                content_type='application/json')
+
+    smart_update_untappd()
+
+    beer1 = Beer.objects.get(vmp_id=12611502)
+    beer2 = Beer.objects.get(vmp_id=12512002)
+
+    assert beer1.untpd_id == 1320463
+    assert beer2.untpd_id == None
