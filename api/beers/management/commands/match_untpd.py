c3c05e907b72af5f5687e3235e5e0a699e7b6eda
Håvard Kråkenes <haavardnk@outlook.com>
2021-06-10 20:43:30 +0200

    Tune untappd matching algorithm
    Fix bug in match untappd

--- api/beers/management/commands/match_untpd.py
+++ api/beers/management/commands/match_untpd.py
@@ -4,89 +4,120 @@ from fuzzywuzzy import process, fuzz
 from beers.models import Beer, ExternalAPI, MatchFilter
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     # Matches beers from beername to untappd ID.
 
     def handle(self, *args, **options):
-        untappd = ExternalAPI.objects.get(name='untappd')
+        untappd = ExternalAPI.objects.get(name="untappd")
         api_client_id = untappd.api_client_id
         api_client_secret = untappd.api_client_secret
         baseurl = untappd.baseurl
 
-        beers = Beer.objects.filter(untpd_id__isnull=True, match_manually=False, active=True)
+        beers = Beer.objects.filter(
+            untpd_id__isnull=True, match_manually=False, active=True
+        )
 
         filters = []
         for f in MatchFilter.objects.all():
             filters.append(f.name)
         filters.sort(key=len, reverse=True)
-        
+
         brewery_list = []
-        api_remaining = '100'
-        matched_beers = 0
+        api_remaining = "100"
+        matched = 0
+        failed = 0
+        failed_beers = []
 
         for beer in beers:
-            # Stop if no api calls remaining
-            if int(api_remaining) <= 5:
-                break
-
             # Make query string
             querystring = beer.vmp_name
 
             # Remove collab brewery
-            if ' x ' in querystring:
+            if " x " in querystring:
                 querywords = querystring.split()
-                index = querywords.index('x')
-                querystring = ' '.join(querywords[:index]+querywords[index+2 :]) 
+                index = querywords.index("x")
+                querystring = " ".join(querywords[:index] + querywords[index + 2 :])
 
             # Remove filter words (only if the word is more than two words long)
             if len(querystring.split()) > 2:
                 for filter_word in filters:
                     if filter_word in querystring.lower():
                         querywords = querystring.split()
-                        resultwords  = [word for word in querywords if word.lower() not in filter_word.split()]
-                        querystring = ' '.join(resultwords)     
-            query = querystring 
+                        resultwords = [
+                            word
+                            for word in querywords
+                            if word.lower() not in filter_word.split()
+                        ]
+                        querystring = " ".join(resultwords)
+            query = querystring
 
-            url = baseurl+"search/beer?client_id="+api_client_id+"&client_secret="+api_client_secret+"&q="+quote(query)+"&limit=5"
+            url = (
+                baseurl
+                + "search/beer?client_id="
+                + api_client_id
+                + "&client_secret="
+                + api_client_secret
+                + "&q="
+                + quote(query)
+                + "&limit=5"
+            )
 
             try:
                 request = requests.get(url)
                 response = json.loads(request.text)
-                api_remaining = request.headers['X-Ratelimit-Remaining']
+                api_remaining = request.headers["X-Ratelimit-Remaining"]
             except:
-                print("Why?")
                 break
 
             try:
                 # Use fuzzywuzzy to assert matches instead of just taking top result
                 beer2match = query
                 options = []
-                
-                for r in response['response']['beers']['items']:
+
+                for r in response["response"]["beers"]["items"]:
                     # Create options by appending first word in brewery name + beer name
-                    options.append(r['brewery']['brewery_name'].split()[0]+" "+r['beer']['beer_name'])
-                best_match = process.extractOne(beer2match,options,scorer=fuzz.ratio)
+                    options.append(
+                        r["brewery"]["brewery_name"].split()[0]
+                        + " "
+                        + r["beer"]["beer_name"]
+                    )
+                best_match = process.extractOne(beer2match, options, scorer=fuzz.ratio)
 
                 # Only match if match is over 60 (Levenshtein distance)
                 if best_match[1] > 60:
                     # Gets matched beer
-                    match = response['response']['beers']['items'][options.index(best_match[0])]
+                    match = response["response"]["beers"]["items"][
+                        options.index(best_match[0])
+                    ]
 
                     # Updates database
-                    beer.untpd_id = match['beer']['bid']
-                    beer.untpd_url = "https://untappd.com/b/"+match['beer']['beer_slug']+"/"+str(match['beer']['bid'])
+                    beer.untpd_id = match["beer"]["bid"]
+                    beer.untpd_url = (
+                        "https://untappd.com/b/"
+                        + match["beer"]["beer_slug"]
+                        + "/"
+                        + str(match["beer"]["bid"])
+                    )
                     beer.save()
+                    matched += 1
+
                 else:
                     beer.match_manually = True
                     beer.save()
-
-                matched_beers += 1
+                    failed += 1
+                    failed_beers.append(beer)
 
             except:
                 beer.match_manually = True
                 beer.save()
-                continue
+                failed += 1
+                failed_beers.append(beer)
 
-        self.stdout.write(self.style.SUCCESS(f'Successfully matched {matched_beers} beers,'+\
-                                             f' {api_remaining} remaining API calls.'))
-                                             
+            print(f"Matched: {matched} Failed: {failed} API remaining: {api_remaining}")
+
+            # Stop if no api calls remaining
+            if int(api_remaining) <= 5:
+                break
+
+        self.stdout.write(self.style.SUCCESS(f"Successfully matched {matched} beers."))
