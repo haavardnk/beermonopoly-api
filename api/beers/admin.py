d6d0dfc003bebab4a160ffa34f772d4e01e7b52f
Håvard Kråkenes <haavardnk@outlook.com>
2021-06-10 20:56:59 +0200

    Fix algorithms to comply with new vinmonopolet api response
    Add prioritize recheck to api view
    
    
    Remove space from gps coordinates of CSV
    
    
    Fix bug in csv file import

--- api/beers/admin.py
+++ api/beers/admin.py
@@ -1,26 +1,40 @@
 from django.contrib import admin
 from beers.models import Beer, ExternalAPI, Store, Stock, MatchFilter
 
+
 @admin.register(Beer)
 class BeerAdmin(admin.ModelAdmin):
-    list_display = ("vmp_name", "brewery", "untpd_name", "vmp_id", "untpd_id", "rating", "vmp_updated", "untpd_updated", "match_manually")
-    search_fields = ("vmp_name", "brewery", "vmp_id", "untpd_id", "style" )
+    list_display = (
+        "vmp_name",
+        "brewery",
+        "untpd_name",
+        "vmp_id",
+        "untpd_id",
+        "rating",
+        "vmp_updated",
+        "untpd_updated",
+        "match_manually",
+        "active",
+    )
+    search_fields = ("vmp_name", "brewery", "vmp_id", "untpd_id", "style")
+
 
 class StockInline(admin.TabularInline):
     model = Stock
 
+
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
     list_display = ("name", "store_id", "address", "store_updated")
-    inlines = [
-        StockInline
-    ]
+    inlines = [StockInline]
     search_fields = ("name", "store_id")
+
 
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
     list_display = ("store", "beer", "quantity", "stock_updated")
     search_fields = ("store__name", "beer__vmp_name")
+
 
 admin.site.register(ExternalAPI)
 admin.site.register(MatchFilter)
--- api/beers/management/commands/update_stock_from_vmp.py
+++ api/beers/management/commands/update_stock_from_vmp.py
@@ -1,56 +1,72 @@
-import requests, json
+import cloudscraper, xmltodict
 from beers.models import Beer, ExternalAPI, Store, Stock
 from django.utils import timezone
 from django.core.management.base import BaseCommand
 
-def call_api(url, store_id, page):
-    params = {'query':':name-asc:visibleInSearch:true:mainCategory:øl:availableInStores:'+str(store_id)+':','pageSize':100,'currentPage':page}
-    request = requests.get(url = url, params = params)
-    response = json.loads(request.text)
-    total_pages = response['pagination']['totalPages']
 
-    return(response, total_pages)
+def call_api(url, store_id, page):
+    query = (
+        ":name-asc:visibleInSearch:true:mainCategory:øl:availableInStores:"
+        + str(store_id)
+        + ":"
+    )
+    req_url = (
+        url + "?currentPage=" + str(page) + "&fields=FULL&pageSize=100&query=" + query
+    )
+    scraper = cloudscraper.CloudScraper()
+    request = scraper.get(req_url).text
+    response = xmltodict.parse(request)["productCategorySearchPage"]
+    total_pages = response["pagination"]["totalPages"]
+
+    return (response, total_pages)
+
 
 class Command(BaseCommand):
     # Updates the database with all beers from vinmonopolet
 
     def handle(self, *args, **options):
-        baseurl = ExternalAPI.objects.get(name="vinmonopolet").baseurl      
-        url = baseurl+'products/search/'
+        baseurl = ExternalAPI.objects.get(name="vinmonopolet").baseurl
+        url = baseurl + "products/search/"
 
         updated = 0
         created = 0
-
+        deleted = 0
+        stores_updated = 0
         stores = Store.objects.all()
 
-        for store in stores:
+        for store in stores.iterator():
             stocked_beer = []
+
             response, total_pages = call_api(url, store.store_id, 0)
 
             # Update all beers in stock
-            for page in range(0,total_pages):
+            for page in range(0, int(total_pages)):
                 try:
                     response, total_pages = call_api(url, store.store_id, page)
 
-                    for r in response['products']:
+                    for r in response["products"]:
                         # Find beer
-                        beer = Beer.objects.get(vmp_id=int(r['code']))
+                        beer = Beer.objects.get(vmp_id=int(r["code"]))
                         stocked_beer.append(beer)
 
-                        quantity = int(r['availability']['storeAvailability']['mainText'].split()[1])
-                        
+                        quantity = int(
+                            r["availability"]["storeAvailability"]["mainText"].split()[
+                                1
+                            ]
+                        )
+
                         try:
                             stock = Stock.objects.get(store=store, beer=beer)
                             stock.quantity = quantity
                             stock.save()
-                            
+
                             updated += 1
 
                         except Stock.DoesNotExist:
                             stock = Stock.objects.create(
-                            store = store,
-                            beer = beer,
-                            quantity = quantity,
+                                store=store,
+                                beer=beer,
+                                quantity=quantity,
                             )
 
                             created += 1
@@ -58,9 +74,19 @@ class Command(BaseCommand):
                 except:
                     continue
 
-            # Remove all beers no longer in stock
+            # Remove all beers no longer in stock in the store
+            stocks = Stock.objects.filter(store=store).exclude(beer__in=stocked_beer)
+            deleted += stocks.count()
+            stocks.delete()
 
-            Stock.objects.filter(store=store).exclude(beer__in=stocked_beer).delete()
+            stores_updated += 1
 
-        
-        self.stdout.write(self.style.SUCCESS(f'Updated {updated} stocks and created {created} new stocks!'))
+            print(
+                f"Updated: {updated} Created: {created} Deleted: {deleted} Stores updated: {stores_updated}/{len(stores)}"
+            )
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully updated {updated} stocks, created {created} new stocks and deleted {deleted} stocks!"
+            )
+        )
