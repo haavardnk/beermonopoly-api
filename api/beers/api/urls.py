d45395bb7d8f6b2bb54eb3b8d6129704a0b9454d
Håvard Kråkenes <haavardnk@outlook.com>
2021-06-12 15:55:05 +0200

    Add manual match view

--- api/beers/api/urls.py
+++ api/beers/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from beers.api.views import BeerViewSet, StoreViewSet, StockViewSet
+from beers.api.views import BeerViewSet, StoreViewSet, StockViewSet, MatchViewSet
 
 router = DefaultRouter()
-router.register(r"beers", BeerViewSet, basename="beers")
-router.register(r"stores", StoreViewSet, basename="stores")
+router.register(r"beers", BeerViewSet, basename="beer")
+router.register(r"stores", StoreViewSet, basename="store")
 router.register(r"stock", StockViewSet, basename="stock")
+router.register(r"match", MatchViewSet, basename="match")
 
 urlpatterns = [
     path("", include(router.urls)),
