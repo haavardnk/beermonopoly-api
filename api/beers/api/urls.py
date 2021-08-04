b7851589ec83685818a496934ce18a5a31f9edbf
Håvard Kråkenes <haavardnk@outlook.com>
2021-08-04 10:56:42 +0200

    Add API to suggest wrong untappd match

--- api/beers/api/urls.py
+++ api/beers/api/urls.py
@@ -1,12 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from beers.api.views import BeerViewSet, StoreViewSet, StockViewSet, MatchViewSet
+from beers.api.views import (
+    BeerViewSet,
+    StoreViewSet,
+    StockViewSet,
+    MatchViewSet,
+    WrongMatchViewSet,
+)
 
 router = DefaultRouter()
 router.register(r"beers", BeerViewSet, basename="beer")
 router.register(r"stores", StoreViewSet, basename="store")
 router.register(r"stock", StockViewSet, basename="stock")
 router.register(r"match", MatchViewSet, basename="match")
+router.register(r"wrongmatch", WrongMatchViewSet, basename="wrongmatch")
 
 urlpatterns = [
     path("", include(router.urls)),
