192bb84e57df7a6d0ae5284a8cf2c0a202dd17f2
Håvard Kråkenes <haavardnk@outlook.com>
2021-08-29 14:50:13 +0200

    Add task to remove manual match

---
+++ api/beers/management/commands/remove_match_manually.py
@@ -0,0 +1,12 @@
+from beers.models import Beer
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        beers = Beer.objects.all()
+        for beer in beers:
+            beer.match_manually = False
+            beer.save()
+
+        self.stdout.write(self.style.SUCCESS(f"Removed all match manually flags"))
