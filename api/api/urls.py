24f71b70f2218c9b0e3c3ed675b8f822ed410805
Håvard Nordhus Kråkenes <haavardnk@outlook.com>
2024-02-01 19:54:57 +0800

    Add django_admin_shell

--- api/api/urls.py
+++ api/api/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 urlpatterns = [
+    re_path(r"^admin/shell/", include("django_admin_shell.urls")),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("", include("beers.api.urls")),
