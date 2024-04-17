e95fc0136e80a6c99036d64040b93a42bd686f67
Håvard Nordhus Kråkenes <haavardnk@outlook.com>
2024-04-17 22:56:39 +0800

    Update gunicorn workers to 5

--- api/run.sh
+++ api/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 nohup python manage.py qcluster &
-exec gunicorn --limit-request-line 8000 --timeout 60 --bind :7000 --workers 3 api.wsgi:application
+exec gunicorn --limit-request-line 8000 --timeout 60 --bind :7000 --workers 5 api.wsgi:application
