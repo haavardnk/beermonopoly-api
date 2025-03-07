878191a6c9ea904e474086fc233d47d6f4cce7f7
Håvard Kråkenes <hakr@equinor.com>
2025-03-07 12:14:03 +0100

    feat: migrate database on run

--- api/run.sh
+++ api/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# Run database migrations
+uv run manage.py migrate
+
+# Start qcluster in the background
 nohup uv run manage.py qcluster &
+
+# Start Gunicorn server
 uv run gunicorn --limit-request-line 8000 --timeout 60 --bind :7000 --workers 5 api.wsgi:application
