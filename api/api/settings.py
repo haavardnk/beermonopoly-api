84f4233dd3d4f7b8742efb1bab12a58283332757
Håvard Kråkenes <29313194+haavardnk@users.noreply.github.com>
2025-06-24 21:35:38 +0200

    feat: add session authentication

--- api/api/settings.py
+++ api/api/settings.py
@@ -217,6 +217,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_RENDERER_CLASSES": [
